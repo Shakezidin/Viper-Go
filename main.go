46780acaa085c18661236177fc565eff7189e0a2
Shakezidin <sinuzidin@gmail.com>
2023-11-02 08:54:48 +0530

    Viper sample

---
+++ main.go
@@ -0,0 +1,10 @@
+// main.go
+package main
+
+import (
+	"github.com/Shakezidin/Viper-go/initializer" // Update the import path with your actual username or repository name
+)
+
+func main() {
+	initializer.Loadenv()
+}
